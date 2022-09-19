ff2b74bfb018163989cfbfe4e34c08f73093949c
zhuxueqiao <1196326205@qq.com>
2022-09-19 13:47:28 +0800

    test

---
+++ leetcode/src/main/java/pers/xueqiao/algorithms/One.java
@@ -0,0 +1,10 @@
+package pers.xueqiao.algorithms;
+
+/**
+ * url:https://leetcode.cn/problems/two-sum/
+ *
+ * @author xueqiao
+ * @create 2022 - 09 - 19 13:46
+ */
+public class One {
+}
---
+++ leetcode/src/main/java/pers/xueqiao/algorithms/Two.java
@@ -0,0 +1,10 @@
+package pers.xueqiao.algorithms;
+
+/**
+ * url:https://leetcode.cn/problems/add-two-numbers/
+ *
+ * @author xueqiao
+ * @create 2022 - 09 - 19 13:46
+ */
+public class Two {
+}
