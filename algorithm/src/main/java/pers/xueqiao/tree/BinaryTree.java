949558f7b946416e6846028c7f852dac65cb07de
zhuxueqiao <1196326205@qq.com>
2022-09-19 13:42:22 +0800

    test

---
+++ algorithm/src/main/java/pers/xueqiao/tree/BinaryTree.java
@@ -0,0 +1,12 @@
+package pers.xueqiao.tree;
+
+/**
+ * 二叉树
+ *
+ * @author xueqiao
+ * @create 2022 - 09 - 19 13:42
+ */
+public class BinaryTree {
+
+}
+
--- untitled/src/main/java/com/dwd/Sstudeng.java
+++
@@ -1,9 +0,0 @@
-package com.dwd;
-
-/**
- * @author xueqiao
- * @create 2022 - 09 - 19 13:35
- */
-public class Sstudeng {
-
-}
