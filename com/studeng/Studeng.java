fdfc8c738741ad36cc5f8363a7ff5123e44cf49a
zhuxueqiao <1196326205@qq.com>
2022-09-19 13:20:32 +0800

    test

--- com/studeng/Studeng.java
+++ com/studeng/Studeng.java
@@ -5,4 +5,9 @@ package com.studeng;
  * @create 2022 - 09 - 19 13:14
  */
 public class Studeng {
+    private Long id;
+
+    public static void main(String[] args) {
+        System.out.println("123456789");
+    }
 }
